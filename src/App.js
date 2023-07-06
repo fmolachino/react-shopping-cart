24d4180cf9d1556ee4531558f7d91d19a7c7ad4e
fmolachino <fmolachino@gmail.com>
2023-07-06 16:18:18 -0300

    added simulated fetch via timeOut for the data gathering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+
 import { ItemListContainer } from "./components/ItemListContainer";
 import { NavBar } from "./components/NavBar";
 
 export default function App() {
   return (
     <>
-      <NavBar />
-      <ItemListContainer greeting="Welcome to Fer's Shop"/>
-    </>
-  )
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route
+            path="/"
+            element={<ItemListContainer greeting="Welcome to Fer's Shop" />}
+          />
+          <Route
+            path="/category/:id"
+            element={<ItemListContainer greeting="Welcome to Fer's Shop" />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>  
+  );
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,31 @@
 import Container from "react-bootstrap/Container";
+import { useState, useEffect } from "react";
 
+import productData from "../data/products.json";
+import { ItemList } from "../components/ItemList"
 
 //TODO: to be a future page
-export const ItemListContainer = ({greeting}) => (
+export const ItemListContainer = props => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const myPromise = new Promise((resolve, rejected) => {
+      setTimeout(() => {
+         resolve(productData);
+      }, 2000)
+    })
+
+    myPromise.then((result) => setProducts(result));
+  }, [])
+
+  return (
     <Container className="mt-3">
-        <h1>{greeting}</h1>
+      <h1>{props.greeting}</h1>
+      {products.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        products.map(product => <div key={product.id}>{product.name}</div>)
+      )}
     </Container>
-)
+  )
+}
